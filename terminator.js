220093cdd58b7e641b395720d4bd48656bdbd794
BC <analytalica@gmail.com>
2016-05-24 20:26:22 +0100

    Added cat function

--- terminator.js
+++ terminator.js
@@ -1,3 +1,4 @@
+//Create a new Terminator instance targeting a DOM element with a configuration.
 var Terminator = function(element, config) {
     var hiddenField = document.createElement('input');
     hiddenField.setAttribute('type', 'text');
@@ -11,6 +12,8 @@ var Terminator = function(element, config) {
     this.displayField = null;
     this.callback = false;
     this.locked = false;
+    
+    this.programs = {};
     
     hiddenField.addEventListener('input', (function() {
         console.log('Typing!');
@@ -33,20 +36,59 @@ var Terminator = function(element, config) {
     }
 };
 
+//Register a program to a Terminator instance with a function and name.
+Terminator.prototype.register = function(callback, name) {
+    this.programs[name] = callback;
+}
+
+//Makes a linebreak and then prints text.
+Terminator.prototype.writeLine = function(content) {
+    this.lineBreak();
+    var span = document.createElement('span');
+    span.textContent = content;
+    this.element.appendChild(span);
+    return span;
+}
+
+//Makes a linebreak and prints HTML.
+Terminator.prototype.writeHTML = function(content) {
+    this.lineBreak();
+    var div = document.createElement('div');
+    div.innerHTML = content;
+    this.element.appendChild(div);
+    return div;
+}
+
 Terminator.prototype.lineBreak = function() {
     var br = document.createElement('br');
     this.element.appendChild(br);
 }
 
+//Runs a callback, known program, or produces an error.
 Terminator.prototype.run = function(command) {
     this.locked = true;
-    this.lineBreak();
+
+    //If a callback was provided to the prompt, run it once
+    //Note that the callback is responsible for re-prompting when finished
+    if (this.callback) {
+        this.callback(this, command);
+        this.callback = null;
+        return;
+    }
     
-    console.log("Just tried to run: " + command);
-    
-    this.hiddenField.value = '';
-    this.locked = false;
-    this.prompt();
+    //Otherwise, search the known programs list
+    console.log("No callback for command: " + command);
+    var args = command.split(' ');
+    if (args[0] && this.programs[args[0]]) {
+        console.log("Running registered program " + args[0]);
+        this.programs[args[0]](this, command);
+        
+    } else {
+        console.warn("Invalid command: " + command);
+        this.writeLine(command + ": command not found");
+        this.lineBreak();
+        this.prompt();
+    }
 }
 
 Terminator.prototype.autoType = function(command) {
@@ -67,12 +109,12 @@ Terminator.prototype.autoType = function(command) {
     }
     
     setTimeout((function() {
-        //this.locked = false;
-        this.callback ? this.callback(this, this.displayField.textContent) : this.run(this.displayField.textContent);
+        this.run(this.displayField.textContent);
     }).bind(this), i * 40);
 }
 
 Terminator.prototype.prompt = function(prefix, callback) {
+    this.hiddenField.value = '';
     prefix = prefix || this.config.prefix || '~$';
     
     var promptWrapper = document.createElement('span');
