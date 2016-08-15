75ba9be379f884a1716ba6e3eefe16caf5dcd7fd
JC/BC <analytalica@gmail.com>
2016-08-14 22:42:28 -0500

    Added new script files

---
+++ script.js
@@ -0,0 +1,95 @@
+var main = new Terminator(document.getElementById('contentWrapper'), 
+            { 
+                prefix: '<span class="green">isss@utexas</span>:<span class="red">~$</span> ',
+                alwaysFocus: true,
+                autoScroll: (window.innerWidth >= 600)
+            });
+
+window.onresize = function() {
+    console.log("Resizing!");
+    if (window.innerWidth >= 600) {
+        main.config.autoScroll = true;
+    } else {
+        main.config.autoScroll = false;
+    }
+};
+            
+function printClass(term, className) {
+    var cloneTarget = null;
+    if (className && (cloneTarget = document.getElementsByClassName(className))) {
+        if (cloneTarget[0]) {
+            term.writeHTML(cloneTarget[0].innerHTML);
+        } else {
+            term.writeLine("cat: " + className + ": No such file or directory");
+        }
+    }
+}
+
+main.register(function(term, command) {
+    term.writeLine('welcome.txt');
+    term.writeLine('officers.txt');
+    term.writeLine('contact.txt');
+    term.writeLine('nuclear_launch_codes.txt');
+    term.prompt();
+}, 'ls');
+			
+main.register(function(term, command) {
+    command = command.split(' ');
+    var arg = command[1] || '';
+    if (arg.indexOf('.txt') !== -1
+        && arg.indexOf('.txt') === (arg.length - 4))
+        arg = arg.substring(0, arg.indexOf('.txt'));
+        
+    printClass(term, arg);
+    term.prompt();
+    return;
+}, 'cat');
+
+main.register(function(term, command) {
+	printClass(term, 'officers');
+	term.prompt();
+	return;
+}, 'officers');
+
+main.register(function(term, command) {
+	printClass(term, 'contact');
+	term.prompt();
+	return;
+}, 'contact');
+
+main.register(function(term, command) {
+	printClass(term, 'help');
+	term.prompt();
+	return;
+}, ['help', 'man']);
+
+main.register(function(term, command) {
+    printClass(term, 'git');
+    term.prompt();
+    return;
+}, 'git');
+
+main.register(function(term, command) {
+	term.writeLine('/utexas/utcs/orgs/isss');
+	term.prompt();
+}, 'pwd');
+
+main.register(function(term, command) {
+	term.element.innerHTML = '';
+	term.prompt();
+}, 'clear');
+
+main.register(function(term, command) {
+    term.writeLine('This user is not in the cders file. This incident has been reported.');
+    term.prompt();
+}, 'cd');
+
+main.register(function(term, command) {
+    var contentWrapper = document.getElementById('contentWrapper');
+    contentWrapper.classList.toggle('hacker');
+    term.writeLine('Hacker mode: ' + (contentWrapper.classList.contains('hacker') ? 'ENABLED' : 'DISABLED'));
+    term.prompt();
+}, ['hack', 'hacker']);
+
+main.prompt();
+main.autoType('cat welcome.txt', 1000);
